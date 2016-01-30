a1347f0fb8a4f52a9af4ab78de084a78aefb584d
Erko <matteo.ercolani@gmail.com>
2016-01-30 19:38:57 +0100

    fix post login url

--- skills/serializers.py
+++ skills/serializers.py
@@ -25,6 +25,6 @@ class UserSkillExtendedSerializer(serializers.Serializer):
     skill_name = serializers.CharField(max_length=200, source='skill.name')
     user_id = serializers.IntegerField()
     user_email = serializers.EmailField(source='user.email')
-    user_profile_image_url = serializers.CharField(source='user.profile.image.url')
+    user_profile_image_url = serializers.CharField(source='user.profile.get_image_url')
     user_full_name = serializers.CharField(max_length=200, source='user.get_full_name')
 
