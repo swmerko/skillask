4e8967dc63da63a523c2f3d525c61119b88ed8b2
Erko <matteo.ercolani@gmail.com>
2016-02-13 14:34:00 +0100

    fix static

--- SKILLASK/settings.py
+++ SKILLASK/settings.py
@@ -128,7 +128,8 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
