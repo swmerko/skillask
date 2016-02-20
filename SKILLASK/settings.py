e7ea0ab8dcd78258dd4213c37f6e9e9face3b204
Erko <matteo.ercolani@gmail.com>
2016-02-20 22:29:09 +0100

    fix pipeline

--- SKILLASK/settings.py
+++ SKILLASK/settings.py
@@ -119,9 +119,9 @@ REST_FRAMEWORK = {
     ),
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ],
     # 'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
