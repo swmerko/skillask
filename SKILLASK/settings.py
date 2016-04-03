077db7307b099a509b9e111e102a89eb8ab0e3ca
Erko <matteo.ercolani@gmail.com>
2016-04-03 19:54:29 +0200

    migrations

--- SKILLASK/settings.py
+++ SKILLASK/settings.py
@@ -222,10 +222,10 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
 # you run `collectstatic`).
 
-# MEDIAFILES_LOCATION = 'media'
-# MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-# DEFAULT_FILE_STORAGE = 'core.custom_storages.MediaStorage'
-# DATABASES['default'] = dj_database_url.config()
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'core.custom_storages.MediaStorage'
+DATABASES['default'] = dj_database_url.config()
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
