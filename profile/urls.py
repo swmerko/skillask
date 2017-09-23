a72bb6680036b67b6bc851f2956ccfbc7179df8e
Matteo Ercolani <erko@MacBook-Pro-di-Matteo.local>
2017-09-23 19:49:47 +0200

    add_search

--- profile/urls.py
+++ profile/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import add_skills
+from .views import add_skills, add_skill
 
 urlpatterns = [
-    url(r'^add-skills$', add_skills, name='add_skills'),
+    url(r'^add-skills/$', add_skills, name='add_skills'),
+    url(r'^add-skills/(?P<skill_id>\d+)$', add_skill, name='add_skill'),
 ]
--- profile/views.py
+++ profile/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
 from django.shortcuts import render, get_object_or_404
 
 from skills.models import Skill, UserSkill
@@ -6,6 +7,24 @@ from skills.models import Skill, UserSkill
 
 @login_required
 def add_skills(request):
+
+    users_skill = UserSkill.objects.filter(user=request.user)
+
+    context = {
+        'users_skill': users_skill,
+    }
+
+    return render(request, 'profile/add_skills.html', context)
+
+
+@login_required
+def add_skill(request, skill_id=None):
+    if skill_id:
+        skill = get_object_or_404(Skill, pk=skill_id)
+        try:
+            UserSkill.objects.create(user=request.user, skill=skill)
+        except IntegrityError:
+            pass
 
     users_skill = UserSkill.objects.filter(user=request.user)
 
