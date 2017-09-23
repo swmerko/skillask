b1050b54ea5bd1cc641ec729ab050a5e97cac543
Matteo Ercolani <erko@MacBook-Pro-di-Matteo.local>
2017-09-23 18:41:46 +0200

    add_search

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+
+from .views import search
 from .rest import base_search
 
 urlpatterns = [
     url(r'^api/base_search/$', base_search, name='base_search'),
+    url(r'^$', search, name='search'),
+    url(r'^(?P<skill_id>\d+)$', search, name='search_skill_id'),
 ]
---
+++ search/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+
+from skills.models import Skill, UserSkill
+
+
+def search(request, skill_id):
+    skill = get_object_or_404(Skill, pk=skill_id)
+
+    users_skill = UserSkill.objects.filter(skill=skill)
+
+    context = {
+        'skill': skill,
+        'users_skill': users_skill,
+    }
+
+    return render(request, 'search/search.html', context)
